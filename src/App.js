20c1c95c2a8587e9484f19d61d5418c0ceb91f4e
Dutu Paul <dutu_paul15@yahoo.com>
2023-11-19 16:36:01 +0200

    content of website changed

--- src/App.js
+++ src/App.js
@@ -6,24 +6,23 @@ import { Home } from "./pages/home/Home";
 import { Login } from "./pages/login/Login";
 import { Register } from "./pages/login/Register";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import { DetailsPages } from "./pages/details/DetailsPages";
+import { DetailsPages1} from "./pages/details/DetailsPages1";
 import { Account } from "./pages/account/Account";
 import { Create } from "./components/create/Create";
 import { Contact } from "./pages/contact/Contact";
 import DarkMode from "./components/DarkMode/DarkMode";
-
 
 const App = () => {
   return (
     <>
       <Router>
         <Header />
-        <DarkMode/>
+        <DarkMode />
         <Switch>
           <Route exact path="/" component={Home} />
           <Route exact path="/login" component={Login} />
           <Route exact path="/register" component={Register} />
-          <Route exact path="/details/:id" component={DetailsPages} />
+          <Route exact path="/details/:id" component={DetailsPages1} />
           <Route exact path="/account" component={Account} />
           <Route exact path="/create" component={Create} />
           <Route exact path="/contact" component={Contact} />
